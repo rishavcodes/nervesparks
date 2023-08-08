f92da1f9935c50d76dfd7e855c2003b9b7a8f900
rishavcodes <rjnycusa397@gmail.com>
2023-08-08 20:19:20 +0530

    project added

---
+++ src/lib/Components/Sidebar/+Sidebar.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let open = false;
+	export let data
+	const {products} = data;
+	console.log(data)
+</script>
+
+<aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
+	<nav class="p-12 text-xl">
+		{#each products as product}
+			<h3>product.title</h3>
+		{/each}
+	</nav>
+</aside>
+
+<style>
+	aside {
+		left: -100%;
+		transition: left 0.3s ease-in-out
+	}
+	
+	.open {
+		left: 0
+	}
+</style>
